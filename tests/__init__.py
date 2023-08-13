bc2e373bfd7aaf24f79f0aa51ecd9436a9968f0b
FriedrichFroebel <FriedrichFroebel@users.noreply.github.com>
2023-08-13 17:47:46 +0200

    document patch

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,11 @@ from unittest import TestCase  # noqa: F401
 
 
 def patch_deprecation_warning():
+    """
+    Avoid the deprecation warning from `brother_ql.devicedependent`. This has been
+    fixed in the Git version, but not in PyPI one:
+    https://github.com/pklaus/brother_ql/commit/5c2b72b18bcf436c116f180a9147cbb6805958f5
+    """
     original_logger = logging.getLogger("brother_ql.devicedependent").warning
 
     def warn(message, *args, **kwargs):
